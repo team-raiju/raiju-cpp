f6bade0fe23ba32e5ac652ed0188c1fda8707296
Daniel Nery <danielnso97@gmail.com>
2022-03-17 18:43:27 -0300

    Fix build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,12 @@
 bash scripts/cube.sh
 
 mkdir -p build
+/usr/sbin/cmake --no-warn-unused-cli                           \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE                  \
+    -DCMAKE_BUILD_TYPE:STRING=Debug                            \
+    -DCMAKE_C_COMPILER:FILEPATH=/bin/arm-none-eabi-gcc-11.2.0  \
+    -S ${PWD}                                                  \
+    -B ${PWD}/build                                            \
+    -G Ninja
+
 /usr/sbin/cmake --build build --config Debug --target all -j 8
