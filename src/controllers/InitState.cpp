e816ac8c271f7594cec8e40e0e75e4c25db6b77b
marcoabensur <marco.abensur@neofield.com.br>
2022-04-22 13:11:41 -0300

    Change led routine to start only once on start

--- src/controllers/InitState.cpp
+++ src/controllers/InitState.cpp
@@ -15,16 +15,16 @@ void FSM::InitState::enter(FSM* fsm) {
     // fsm->s_buzzer.play_megalovania();
 
     // fsm->s_led.purple_charge();
-    for (int i = 0; i < 16; i++) {
-        fsm->s_led.led_stripe.set_color(i, Color::purple());
-        fsm->s_led.led_stripe.send();
+    // for (int i = 0; i < 16; i++) {
+    //     fsm->s_led.led_stripe.set_color(i, Color::purple());
+    //     fsm->s_led.led_stripe.send();
 
-        fsm->s_buzzer.beep(42);
-        hal::mcu::sleep(42);
-    }
+    //     fsm->s_buzzer.beep(42);
+    //     hal::mcu::sleep(42);
+    // }
 
-    hal::mcu::sleep(200);
-    fsm->s_buzzer.beep(600);
+    // hal::mcu::sleep(200);
+    // fsm->s_buzzer.beep(600);
 }
 
 void FSM::InitState::cycle(FSM* fsm) {
