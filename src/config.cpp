545cc153a8440f346fe7a16c681a68c37eb16f81
marcoabensur <marco.abensur@gmail.com>
2022-07-01 23:39:19 -0300

    Change default mask for distance sensors

--- src/config.cpp
+++ src/config.cpp
@@ -2,7 +2,7 @@
 
 namespace raiju::config {
 
-uint8_t enabledDistanceSensors = 0xFF;
+uint8_t enabledDistanceSensors = 0b01101011; // R, FR, DR, F, DL, FL, L
 uint8_t enabledLineSensors = 0xFF;
 uint8_t reverseSpeed = 100;
 uint8_t reverseTimeMs = 50;
